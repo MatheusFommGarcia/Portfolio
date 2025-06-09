3e82ad36bb557989fbfb68a2955b9c4089b116f3
Matheus Fomm <matheus.fomm.garcia1@gmail.com>
2025-06-09 11:26:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// transformar menu hamburger em x
+const menuHamburger = document.querySelector (
+    '.menu-hamburger');
+    menuHamburger.addEventListener ('click', () => {
+        toggleMenu();
+    });
