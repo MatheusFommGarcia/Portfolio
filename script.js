45c2124e85480247ae6b2ab210e0a34246a5682e
Matheus Fomm Garcia <matheus.fomm.garcia1@gmail.com>
2025-08-18 10:18:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Seleciona o menu hamburguer e o menu de navegação responsiva
+// Transformar menu hamburguer em X
 const menuHamburguer = document.querySelector('.menu-hamburguer');
-const nav = document.querySelector('.nav-responsive');
 
-// Adiciona evento de clique
 menuHamburguer.addEventListener('click', () => {
-    // Alterna a classe "change" no ícone
-    menuHamburguer.classList.toggle('change');
-    
-    // Alterna a classe "active" no menu
-    nav.classList.toggle('active');
+    toggleMenu();
 });
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+}
+
+if (menuHamburguer.classList.contains('change')) {
+    nav.style.display = 'block';
+} else {
+    nav.style.display = 'none';
+}
